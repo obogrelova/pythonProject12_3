ffcae8d3591ad9c60fd70489fe712daae5809376
obogrelova <on.obogrelova@gmail.com>
2025-02-20 11:48:17 +0500

    Update test.py

--- test.py
+++ test.py
@@ -7,11 +7,11 @@ def test_get_cat_image_success(mocker):
     mock_get.return_value.json.return_value = {
         'url': 'https://api.thecatapi.com/v1/images/abcdef123456.jpg'
     }
-    image = get_cat_image()
-    assert image == {'url': 'https://api.thecatapi.com/v1/images/abcdef123456.jpg'}
+    image_data = get_cat_image()
+    assert image_data == {'url': 'https://api.thecatapi.com/v1/images/abcdef123456.jpg'}
 
 def test_get_cat_image_unsuccess(mocker):
     mock_get = mocker.patch('main.requests.get')
     mock_get.return_value.status_code = 404
-    image = get_cat_image()
-    assert image is None
+    image_data = get_cat_image()
+    assert image_data == None
